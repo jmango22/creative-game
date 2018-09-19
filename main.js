5b85ba4a08b6e7f1e65bb65396a0576efb49a1fb
Jonathon Meng <jonathon.meng@domo.com>
2018-09-19 02:33:40 -0600

    Removed all global variables except for one, made game pretty fun with difficulty, and colored blocks

--- main.js
+++ main.js
@@ -1,95 +1,160 @@
 // This is the Main Logic for the Game
 
-
-var PlayerPostion = 0;
-var Obstacles = [];
-var SpawnWaitCount = 0;
-var canvas = document.getElementById('GameBoard');
-
-const BLOCK_VEL = 2;
-const SPAWN_RATE = 10;
-
+var game = CreateGame();
 
 function startGame() {
-  setInterval(function () {
-    updateGameArea()
+  game = CreateGame();
+  game.timer = setInterval(function () {
+    updateGameArea();
+    updateScore();
   }, 100);
 }
 
 function movePlayer() {
-  var y = event.clientY;     // Get the vertical coordinate
-  PlayerPostion = (y - 100)/5;
+  const Y_POS = getYPos(event, game.canvas);
+  game.player.ship = game.player.ship.move(game.player.ship, Y_POS);
 }
 
 function updateGameArea() {
-  context = canvas.getContext('2d');
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  DrawPlayer(PlayerPostion, context);
+  context = game.canvas.getContext('2d');
+  context.clearRect(0, 0, game.canvas.width, game.canvas.height);
+  
+  game.player.ship.draw(game.player.ship, context);
 
-  SpawnWaitCount++;
-  if(SpawnWaitCount === SPAWN_RATE) {
-    Obstacles.push(CreateBlock());
-    SpawnWaitCount = 0;
+  if(game.spawnWait <= 0) {
+    game.obstacles.push(CreateBlock(game.canvas));
+    game.spawnWait = 200/game.player.score;
   }
 
-  for (let i = 0; i<Obstacles.length; i++) {
-    let block = Obstacles[i];
+  for (let i = 0; i<game.obstacles.length; i++) {
+    let block = game.obstacles[i];
     
-    if(block.isOutOfBounds(block.state)) {
-      Obstacles.splice(i, 1); // Removes block from array
-    } 
+    if(block.isOutOfBounds(block)) {
+      game.obstacles.splice(i, 1); // Removes block from array
+    }
 
-    block = Obstacles[i]; // move to the next one
-    block.state = block.move(block.state, BLOCK_VEL);
-    block.draw(block.state, context);
+    block = game.obstacles[i]; // move to the next one
+    block.state = block.move(block);
+    block.draw(block, context);
+
+    if(collision(game.player.ship, block)) {
+      console.log("Hit a block!");
+      clearInterval(game.timer);
+      //gameOver();
+    }
+  }
+
+  game.spawnWait = game.spawnWait-1;
+  game.player.score = game.player.score+1;
+}
+
+function updateScore() {
+  document.getElementById('ScoreBoard').innerHTML = `score: ${game.player.score}`;
+}
+
+// Game Layout
+
+function CreateGame() {
+  return {
+    // Game Variables
+    spawnWait: 8,
+
+    // Game Peices
+    canvas: document.getElementById('GameBoard'),
+    obstacles: [],
+    player: CreatePlayer()
   }
 }
 
-function DrawPlayer(YAxis, context) {
-  if (context) {
-    context.beginPath();
-    context.moveTo(20, 10 + YAxis);
-    context.lineTo(10, 13 + YAxis);
-    context.lineTo(10, 7 + YAxis);
-    context.fill();
+function CreatePlayer() {
+  return {
+    score: 0,
+    ship: CreateShip()
   }
 }
 
-function CreateBlock() {
+function CreateShip() {
+  return {
+    y: 0,
+    topPoint: {x: 10},
+    frontPoint: {x: 20},
+    bottomPoint: {x: 10},
+
+    move: (ship, y) => {
+      ship.y = y;
+
+      ship.topPoint.y = y + 13;
+      ship.frontPoint.y = y + 10;
+      ship.bottomPoint.y = y + 7;
+      
+      return ship;
+    },
+
+    draw: (ship, context) => {
+      context.fillStyle = "black";
+      context.beginPath();
+      context.moveTo(ship.frontPoint.x, ship.frontPoint.y);
+      context.lineTo(ship.topPoint.x, ship.topPoint.y);
+      context.lineTo(ship.bottomPoint.x, ship.bottomPoint.y);
+      context.fill();
+    }
+  }
+}
+
+function CreateBlock(canvas) {
   const minWidth = 5;
   const maxWidth = 15;
   const minHeight = 5;
   const maxHeight = 15;
+  const minVel = 1;
+  const maxVel = 5;
   
-  let block = {
-    state: {
-      x: canvas.width,
-      y: getRandomInt(0, canvas.height),
-      width: getRandomInt(minWidth, maxWidth),
-      height: getRandomInt(minHeight, maxHeight)
+  return {
+    x: canvas.width,
+    y: getRandomInt(0, canvas.height),
+    width: getRandomInt(minWidth, maxWidth),
+    height: getRandomInt(minHeight, maxHeight),
+    velocity: (Math.random() * (maxVel - minVel) + minVel),
+    color: '#'+Math.floor(Math.random()*16777215).toString(16), // Generate random color code, cause why not?
+
+    move: (block) => {
+      block.x = block.x - block.velocity;
+      return block;
     },
 
-    move: (state, vel) => {
-      state.x = state.x - vel;
-      return state;
-    },
-
-    draw: (state, context) => {
+    draw: (block, context) => {
       if(context) {
-        context.fillRect(state.x, state.y, state.width, state.height);
+        context.fillStyle = block.color;
+        context.fillRect(block.x, block.y, block.width, block.height);
       }
     },
 
-    isOutOfBounds: (state) => {
-      if ((state.x + state.width) < 0) {
+    isOutOfBounds: (block) => {
+      if ((block.x + block.width) < 0) {
         return true;
       }
     }
   }
-  
-  return block;
-} 
+}
+
+// Helper math/geometry funtions
+
+function collision(ship, block) {
+  return pointInRect(ship.topPoint, block) || pointInRect(ship.frontPoint, block) || pointInRect(ship.bottomPoint, block);
+}
+
+function pointInRect(point, rect) {
+  if(point.x >= rect.x && point.x <= (rect.x + rect.width) && point.y >= rect.y && (point.y <= (rect.y + rect.height))) {
+    return true;
+  }
+  return false;
+}
 
 function getRandomInt(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getYPos(event, canvas) {
+  const rect = canvas.getBoundingClientRect();
+  return Math.floor(event.clientY - rect.top);
 }
